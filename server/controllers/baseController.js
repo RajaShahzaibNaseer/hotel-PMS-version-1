20d18debfc67713737becc4e07bd7576c2187214
RajaShahzaibNaseer <shahzaib.naseer.work2@gmail.com>
2026-03-04 16:54:58 +0500

    created blueprint class for controllers

---
+++ server/controllers/baseController.js
@@ -0,0 +1,41 @@
+import supabase from "../utils/supabase";
+
+class BaseController {
+    constructor (tableName)
+    {
+        this.tableName = tableName;
+    }
+
+    async create(request , result)
+    {
+        const {data,error} = await supabase.from(this.tableName).insert(request.body);
+        if (error) return result.json({error : error.message});
+        result.status(201).json(data);
+    }
+
+    async getAll(result) {
+        const {data, error} = await supabase.from(this.tableName).select("*");
+        if (error) return result.json({error : error.message});
+        result.status(201).json(data);
+    }
+
+    async getOne(request, result) {
+        const {data, error} = await supabase.from(this.tableName).select("*").eq("id",request.params.id).single();
+        if (error) return result.json({error : error.message});
+        result.status(201).json(data);
+    }
+
+    async update(request , result)
+    {
+        const {data,error} = await supabase.from(this.tableName).update(request.body).eq("id",request.params.id).select();
+        if (error) return result.json({error : error.message});
+        result.status(201).json(data);
+    }
+
+    async delete(request, result)
+    {
+        const {data, error} = await supabase.from(this.tableName).delete().eq("id",request.params.id);
+        if (error) return result.json({error : error.message});
+        result.status(201).json(data);
+    }
+}
